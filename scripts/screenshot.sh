4e64da27dc9c63cc1d11a851f600bc6f210dc112
Peter Doherty <pdoherty@protonmail.com>
2018-12-20 12:17:05 -0500

    add notify-send notification to screenshot script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d\ %H:%M:%S`
-import -window root "/media/USB1TB/WTF/Dropbox/Photos/screenshot/screenshot $DATE.png"
+DEST="/media/USB1TB/WTF/Dropbox/Photos/screenshot/screenshot-$DATE.png"
+import -window root "/media/USB1TB/WTF/Dropbox/Photos/screenshot/screenshot-$DATE.png" # TODO: why can't I use $DEST here?
+DISPLAY=:0 notify-send -u low -t 6666 "Screenshot saved to $DEST"
