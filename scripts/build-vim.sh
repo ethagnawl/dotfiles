926488397eb7fd6d5465ea65290b1fe6d19ae065
Peter Doherty <pdoherty@protonmail.com>
2019-05-08 10:30:06 -0400

    SCRIPTS - fix linebreaks; temp make hardcode vim81; set bash strict mode

--- scripts/build-vim.sh
+++ scripts/build-vim.sh
@@ -1,22 +1,28 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 git clone git@github.com:vim/vim.git ~/vim
-sudo apt-get remove vim
+sudo apt-get remove -y vim
 cd ~/vim
 sudo make distclean
-./configure --with-features=huge\
+./configure\
+  --with-features=huge\
   --enable-multibyte\
-  --enable-rubyinterp=yes
-  --enable-pythoninterp=yes
-  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu #non-standard!
-  --enable-python3interp=yes
-  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu #non-standard!
-  --enable-perlinterp=yes
-  --enable-luainterp=yes
-  --enable-gui=gtk2
-  --enable-cscope
-  --prefix=/usr
-make VIMRUNTIMEDIR=/usr/local/share/vim/vimXXX # must match version being installed!
+  --enable-rubyinterp=yes\
+  --enable-pythoninterp=yes\
+  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu\ #non-standard!
+  --enable-python3interp=yes\
+  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu\ #non-standard!
+  --enable-perlinterp=yes\
+  --enable-luainterp=yes\
+  --enable-gui=gtk2\
+  --enable-cscope\
+  --prefix=/usr/local
+
+# TODO: sniff out/set appropriate version
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
+
 sudo apt-get install checkinstall
 sudo checkinstall
 sudo apt-mark hold vim
