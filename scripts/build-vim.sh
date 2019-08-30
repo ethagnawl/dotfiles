353bec9cafd8306b369447b978195872b3e61361
Peter Doherty <pdoherty@protonmail.com>
2019-08-30 14:00:07 -0400

    SCRIPTS - update build-vim script

--- scripts/build-vim.sh
+++ scripts/build-vim.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 
+# TODO: clone repository to home directory
+# TODO: more robust removal of potential vim/accessories (https://superuser.com/a/844627/140742)
+# TODO: sniff out appropriate interpreter paths (python)
+# TODO: sniff out/set appropriate version
+
 set -euo pipefail
 
-git clone git@github.com:vim/vim.git ~/vim
-sudo apt-get remove -y vim
-cd ~/vim
-sudo make distclean
-./configure\
-  --with-features=huge\
-  --enable-multibyte\
-  --enable-rubyinterp=yes\
-  --enable-pythoninterp=yes\
-  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu\ #non-standard!
-  --enable-python3interp=yes\
-  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu\ #non-standard!
-  --enable-perlinterp=yes\
-  --enable-luainterp=yes\
-  --enable-gui=gtk2\
-  --enable-cscope\
-  --prefix=/usr/local
+sudo apt-get install checkinstall
 
-# TODO: sniff out/set appropriate version
+sudo apt-get remove -y vim || echo "vim not installed"
+sudo apt-get remove -y vim my-vim || echo "my-vim not installed"
+
+git clone git@github.com:vim/vim.git ~/projects/vim || echo "repository already exists"
+cd ~/projects/vim
+git pull origin master
+
+sudo make distclean
+
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=/usr/local
+
 make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
 
-sudo apt-get install checkinstall
-sudo checkinstall
+sudo checkinstall -y --pkgname=my-vim
 sudo apt-mark hold vim
+sudo vim --cmd 'helptags $VIMRUNTIME/doc' --cmd 'qall' # generate help tags
